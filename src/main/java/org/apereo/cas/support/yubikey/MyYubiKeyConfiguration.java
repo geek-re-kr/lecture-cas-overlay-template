d5ff29881a612016c1c99b38752d00d5e144edcb
CEE <archmagece@gmail.com>
2021-02-18 22:41:06 +0900

    yubikey fail

---
+++ src/main/java/org/apereo/cas/support/yubikey/MyYubiKeyConfiguration.java
@@ -0,0 +1,17 @@
+//package org.apereo.cas.support.yubikey;
+//
+//import org.apereo.cas.adaptors.yubikey.JpaYubiKeyAccount;
+//import org.apereo.cas.configuration.CasConfigurationProperties;
+//import org.springframework.boot.context.properties.EnableConfigurationProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration("myYubiKeyConfiguration")
+//@EnableConfigurationProperties(CasConfigurationProperties.class)
+//public class MyYubiKeyConfiguration {
+//
+//  @Bean
+//  public YubiKeyAccountValidator yubiKeyAccountValidator() {
+//      return JpaYubiKeyAccount
+//  }
+//}
